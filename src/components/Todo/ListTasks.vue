b5b5a88235bac48dacb26ab12c26b01d578c00c6
Oleg Maris <hipp55@ukr.net>
2022-05-30 17:38:05 +0300

    Initial commit

---
+++ src/components/Todo/ListTasks.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-list class="pt-0" flat>
+    <draggable
+      handle=".handle"
+      v-model='tasks'
+    >
+      <task
+        v-for="task in tasks"
+        :key="task.id"
+        :task="task"
+      />
+    </draggable>
+  </v-list>
+</template>
+
+<script>
+import draggable from "vuedraggable";
+
+export default {
+  name: "ListTasks",
+  computed: {
+    tasks: {
+      get() {
+        return this.$store.getters.tasksFiltered;
+      },
+      set(value) {
+        this.$store.dispatch("setTasks", value);
+      },
+    },
+  },
+  components: {
+    task: require("@/components/Todo/Task.vue").default,
+    draggable,
+  },
+};
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About {{ $store.state.appTitle }}</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo nesciunt
+      adipisci, quod esse doloremque, porro natus cum sunt sapiente modi, dolor
+      vitae animi ullam inventore ducimus debitis aperiam et amet.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo nesciunt
+      adipisci, quod esse doloremque, porro natus cum sunt sapiente modi, dolor
+      vitae animi ullam inventore ducimus debitis aperiam et amet.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo nesciunt
+      adipisci, quod esse doloremque, porro natus cum sunt sapiente modi, dolor
+      vitae animi ullam inventore ducimus debitis aperiam et amet.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo nesciunt
+      adipisci, quod esse doloremque, porro natus cum sunt sapiente modi, dolor
+      vitae animi ullam inventore ducimus debitis aperiam et amet.
+    </p>
   </div>
 </template>
